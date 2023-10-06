f3ec234dc231c27aa88eee9de2bdeb555be316c1
GerasimosArapis <146742456+GerasimosArapis@users.noreply.github.com>
2023-10-06 12:20:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,1 @@
-import streamlit
-
-streamlit.title('My parents New Healthy Diet')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+import pandas
